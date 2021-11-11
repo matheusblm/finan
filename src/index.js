e6d99695acd8ce66840d9ec7157635877675b3e5
Victor Scherer <victor_scherer@hotmail.com>
2021-11-11 13:34:51 -0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import AppProvider from "./providers";
+import { theme } from "./style/theme";
 ReactDOM.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ src/pages/Limit/index.js
@@ -0,0 +1,10 @@
+import { Flex, Box, Stack } from '@chakra-ui/react';
+
+const SpendLimit = () => {
+    return (
+        <Stack>Oi
+        </Stack>
+    )
+};
+
+export default SpendLimit;
