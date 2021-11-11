4b45c94eba97b19308d9a1a38377d0b4abbd9115
Felipe Silveira <felipelarson@msn.com>
2021-11-11 11:51:45 +0000

    feat: criação do cabeçalho do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,4 +1,5 @@
-import { Center, Flex, HStack, Stack, Wrap, WrapItem } from "@chakra-ui/react"
+import { Button, Center, Flex, HStack, Icon, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react"
+import { FaPlus, FaMinus } from "react-icons/fa";
 
 export const Dashboard = () => {
     return (
@@ -6,8 +7,8 @@ export const Dashboard = () => {
             <Flex h="106px" bg="#16425B">Header</Flex>
             <Center>
                 <Stack py={4} w={"1047px"}>
-                    <Flex h="230px" bgGradient="linear(to-l, #B7C5DE 0%, rgba(255, 255, 255, 0.44) 99.65%)" p={2} borderRadius="lg" overflow="auto" boxShadow="lg" >
-                        Teste
+                    <Flex h="230px" bgGradient="linear(to-l, blue.500 0%, gray.50 99.65%)" p={2} borderRadius="lg" overflow="auto" boxShadow="lg" >
+                        <HeaderDashboard />
                     </Flex>
                     <HStack>
                         <Stack w="45%" h="548px">
@@ -69,5 +70,50 @@ export const Dashboard = () => {
                 </Stack> */}
             </Center>
         </>
+    )
+}
+
+const HeaderDashboard = () => {
+    return (
+        <Flex>
+            <Flex direction="column" justify="space-around" px={4}>
+                <Text color="gray.600" fontSize="lg">Bem Vindo,<br /> Usuário</Text>
+                <HStack spacing={4}>
+                    <Stack bg="white" borderRadius="lg" py={2} px={6}>
+                        <Text fontSize="sm" color="gray.600">Receita Mensal:</Text>
+                        <Text color="green.300" fontSize="sm">R$ 1.000.000,00</Text>
+                    </Stack>
+                    <Stack bg="white" borderRadius="lg" py={2} px={6}>
+                        <Text fontSize="sm" color="gray.600">Despesa Mensal:</Text>
+                        <Text color="red.300" fontSize="sm">R$ 1.000,00</Text>
+                    </Stack>
+                </HStack>
+            </Flex>
+            <Flex direction="column" px={4} borderX="2px solid #9e9ea7">
+                <Stack bg="white" borderRadius="lg" py={4} px={8} >
+                    <Text fontSize="sm" color="gray.600">Saldo Total:</Text>
+                    <Text fontSize="sm">R$ 1.000.000,00</Text>
+                </Stack>
+            </Flex>
+            <Flex direction="column" justify="center">
+                <Stack py={4} px={8} spacing={12} >
+                    <Text color="gray.600" fontSize="lg" fontWeight="bold">Acesso Rápido:</Text>
+                    <HStack>
+                        <Button bg="white" w="94px" h="70">
+                            <Flex direction="column" align="center" justify="center">
+                                <Icon bg="green.300" color="white" w="40px" h="40px" p={2} borderRadius="lg" as={FaPlus} mb="4px" />
+                                <Text fontWeight="medium" fontSize="sm" color="gray.600">Receita</Text>
+                            </Flex>
+                        </Button>
+                        <Button bg="white" w="94px" h="70">
+                            <Flex direction="column" align="center" justify="center">
+                                <Icon bg="red.300" color="white" w="40px" h="40px" p={2} borderRadius="lg" as={FaMinus} mb="4px" />
+                                <Text fontWeight="medium" fontSize="sm" color="gray.600">Despesa</Text>
+                            </Flex>
+                        </Button>
+                    </HStack>
+                </Stack>
+            </Flex>
+        </Flex >
     )
 }
