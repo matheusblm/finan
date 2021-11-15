43d3368e69d1d55b852bd89db57563d1a01bd5b2
Felipe Silveira <felipelarson@msn.com>
2021-11-15 10:14:33 +0000

    feat: criação do provider da Dashboard

---
+++ src/providers/Dashboard/index.js
@@ -0,0 +1,51 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import api from '../../service/api'
+
+export const DashboardContext = createContext({});
+
+export const DashboardProvider = ({ children }) => {
+    const [receive, setReceive] = useState([]);
+    const [spend, setSpend] = useState([]);
+    const { authToken } = 'useAuth()'
+
+
+    const getAllReceive = () => {
+        api
+            .get("/receive/", {
+                headers: {
+                    Authorization: `Bearer ${authToken}`,
+                }
+            })
+            .then((res) => {
+                res.data.length !== 0 && setReceive(res.data)
+            })
+            .catch(err => console.log(err.message))
+    }
+
+    const getAllSpend = () => {
+        api
+            .get("/spend/", {
+                headers: {
+                    Authorization: `Bearer ${authToken}`,
+                }
+            })
+            .then((res) => {
+                res.data.length !== 0 && setSpend(res.data)
+            })
+            .catch(err => console.log(err.message))
+    }
+
+    useEffect(() => {
+        getAllReceive()
+        getAllSpend()
+        // eslint-disable-next-line
+    }, [])
+
+    return (
+        <DashboardContext.Provider value={{ receive, spend, getAllReceive, getAllSpend }}>
+            {children}
+        </DashboardContext.Provider>
+    )
+}
+
+export const useListDashboard = () => useContext(DashboardContext);
