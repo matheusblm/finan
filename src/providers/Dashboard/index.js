8803ebcd91d2062bff3d7c79d20b4fb766e4ceaa
Felipe Silveira <felipelarson@msn.com>
2021-11-16 12:50:42 +0000

    refactor: mudança da condições de true false

--- src/providers/Dashboard/index.js
+++ src/providers/Dashboard/index.js
@@ -35,8 +35,8 @@ export const DashboardProvider = ({ children }) => {
             .catch(err => console.log(err.message))
     }
 
-    const newReceive = receive.filter(item => item.type === true)
-    const newSpend = spend.filter(item => item.type === true)
+    const newReceive = receive.filter(item => item.type === false)
+    const newSpend = spend.filter(item => item.type === false)
 
     const spendTotal = newSpend.reduce((acc, bill) => acc + bill.value, 0)
     const receiveTotal = newReceive.reduce((acc, bill) => acc + bill.value, 0)
